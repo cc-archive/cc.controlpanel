7c6ccbed322420558cea3f816b5e5765f4317708
Christopher Allan Webber <cwebber@dustycloud.org>
2011-03-23 17:20:22 -0500

    Server-specific actions:  - restricted clear_cache to live  - added a useful _on_what() function that figures out what group you    are in (doesn't work for servers in multiple groups?)  - update_ccengine is contextually aware of live/devel directories

--- fabfile.py
+++ fabfile.py
@@ -1,23 +1,46 @@
-from fabric.api import *
+import os
 import urllib
+
+from fabric.api import *
+from fabric.colors import yellow
 
 
 env.roledefs['live'] = ['webadmin@a5.creativecommons.org']
 env.roledefs['devel'] = ['webadmin@a7.creativecommons.org']
 
 
-def test_live_devel():
-    if env.host_string in env.roledefs['live']:
-        print "%s is in live" % env.host
-    elif env.host_string in env.roledefs['devel']:
-        print "%s is in devel" % env.host
+CCENGINE_LIVE_DIR = '/var/www/creativecommons.org/cc.engine_sanity/'
+CCENGINE_DEVEL_DIR = '/var/www/staging.creativecommons.org/cc.engine_stage/'
+
+
+def _on_what(host_string=None, roledefs=None):
+    """
+    Return which host_string this host is on.
+
+    (Doesn't work for servers in multiple groups?)
+
+    Args:
+     - host_string: the host string; defaults to env.host_string
+     - roledefs: the actual roldef groups, defaults to env.roledefs
+    """
+    host_string = host_string or env.host_string
+    roledefs = roledefs or env.roledefs
+
+    for group in roledefs.keys():
+        if host_string in roledefs[group]:
+            return group
 
 
 def update_ccengine():
-    with cd('/var/www/creativecommons.org/cc.engine_sanity/cc.engine'):
+    if _on_what() == 'devel':
+        ccengine_basedir = CCENGINE_DEVEL_DIR
+    else:
+        ccengine_basedir = CCENGINE_LIVE_DIR
+
+    with cd(os.path.join(ccengine_basedir, 'cc.engine')):
         run('git pull')
 
-    with cd('/var/www/creativecommons.org/cc.engine_sanity'):
+    with cd(ccengine_basedir):
         run('./bin/buildout')
     run('sudo /etc/init.d/apache2 reload')
 
@@ -26,6 +49,7 @@ def update_ccengine():
         run('git pull')
 
 
+@roles('live')
 def clear_cache():
     with cd('/var/www/creativecommons.org/cc.engine_sanity'):
         run('rm -rf cache/licenses')
