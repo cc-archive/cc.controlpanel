a96074053a5257c5f06e4b701b4d9dc66381e9f4
Christopher Allan Webber <cwebber@dustycloud.org>
2011-03-23 11:13:19 -0500

    Added license.rdf updating to update_ccengine and a clear_cache command

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,10 @@
 from fabric.api import *
 import urllib
 
+
 env.roledefs['live'] = ['webadmin@a5.creativecommons.org']
 env.roledefs['devel'] = ['webadmin@a7.creativecommons.org']
+
 
 def test_live_devel():
     if env.host_string in env.roledefs['live']:
@@ -10,10 +12,20 @@ def test_live_devel():
     elif env.host_string in env.roledefs['devel']:
         print "%s is in devel" % env.host
 
-def update_ccengine(clear_cache=False):
+
+def update_ccengine():
     with cd('/var/www/creativecommons.org/cc.engine_sanity/cc.engine'):
         run('git pull')
 
     with cd('/var/www/creativecommons.org/cc.engine_sanity'):
         run('./bin/buildout')
     run('sudo /etc/init.d/apache2 reload')
+
+    # Update license RDF
+    with cd('/var/www/creativecommons.org/www/license.rdf'):
+        run('git pull')
+
+
+def clear_cache():
+    with cd('/var/www/creativecommons.org/cc.engine_sanity'):
+        run('rm -rf cache/licenses')
